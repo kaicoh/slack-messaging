670909512be1d98a9b11ef25de4bbc74993ff6e2
Kanji Tanaka <ktanaka@ichain.co.jp>
2023-02-25 12:35:54 +0900

    Fix Image

--- src/blocks/elements/image.rs
+++ src/blocks/elements/image.rs
@@ -1,5 +1,28 @@
 use serde::Serialize;
 
+/// [Image element](https://api.slack.com/reference/block-kit/block-elements#image)
+/// representation.
+///
+/// # Example
+///
+/// ```
+/// use slack_messaging::blocks::elements::Image;
+/// use serde_json::json;
+///
+/// let image = Image::new()
+///     .set_image_url("http://placekitten.com/700/500")
+///     .set_alt_text("Multiple cute kittens");
+///
+/// let expected = json!({
+///     "type": "image",
+///     "image_url": "http://placekitten.com/700/500",
+///     "alt_text": "Multiple cute kittens"
+/// });
+///
+/// let image_json = serde_json::to_value(image).unwrap();
+///
+/// assert_eq!(image_json, expected);
+/// ```
 #[derive(Debug, Serialize)]
 pub struct Image {
     #[serde(rename = "type")]
@@ -10,19 +33,57 @@ pub struct Image {
     alt_text: String,
 }
 
-impl Image {
-    pub fn new<S, T>(alt: S, url: T) -> Self
-    where
-        S: Into<String>,
-        T: Into<String>,
-    {
+impl Default for Image {
+    fn default() -> Self {
         Self {
             kind: "image",
-            image_url: url.into(),
-            alt_text: alt.into(),
+            image_url: "".into(),
+            alt_text: "".into(),
         }
     }
+}
 
+impl Image {
+    /// Constructs a Image element with empty values.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Image;
+    /// use serde_json::json;
+    ///
+    /// let image = Image::new();
+    ///
+    /// let expected = json!({
+    ///     "type": "image",
+    ///     "image_url": "",
+    ///     "alt_text": ""
+    /// });
+    ///
+    /// let image_json = serde_json::to_value(image).unwrap();
+    ///
+    /// assert_eq!(image_json, expected);
+    /// ```
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Sets image_url field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Image;
+    /// use serde_json::json;
+    ///
+    /// let image = Image::new().set_image_url("http://placekitten.com/700/500");
+    ///
+    /// let expected = json!({
+    ///     "type": "image",
+    ///     "image_url": "http://placekitten.com/700/500",
+    ///     "alt_text": ""
+    /// });
+    ///
+    /// let image_json = serde_json::to_value(image).unwrap();
+    ///
+    /// assert_eq!(image_json, expected);
+    /// ```
     pub fn set_image_url<T: Into<String>>(self, url: T) -> Self {
         Self {
             image_url: url.into(),
@@ -30,6 +91,24 @@ impl Image {
         }
     }
 
+    /// Sets alt_text field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Image;
+    /// use serde_json::json;
+    ///
+    /// let image = Image::new().set_alt_text("Multiple cute kittens");
+    ///
+    /// let expected = json!({
+    ///     "type": "image",
+    ///     "image_url": "",
+    ///     "alt_text": "Multiple cute kittens"
+    /// });
+    ///
+    /// let image_json = serde_json::to_value(image).unwrap();
+    ///
+    /// assert_eq!(image_json, expected);
+    /// ```
     pub fn set_alt_text<T: Into<String>>(self, alt: T) -> Self {
         Self {
             alt_text: alt.into(),
