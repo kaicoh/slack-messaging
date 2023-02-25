91e8f39fe14c3bce567d5f4e5e94460bac3d713d
Kanji Tanaka <ktanaka@ichain.co.jp>
2023-02-25 10:10:26 +0900

    Fix CheckboxGroup

--- src/blocks/elements/checkbox_group.rs
+++ src/blocks/elements/checkbox_group.rs
@@ -1,6 +1,51 @@
 use super::{ConfirmationDialog, Opt};
 use serde::Serialize;
 
+/// [Checkbox group](https://api.slack.com/reference/block-kit/block-elements#checkboxes)
+/// representation.
+///
+/// # Example
+///
+/// ```
+/// use slack_messaging::blocks::elements::{CheckboxGroup, Opt};
+/// use serde_json::json;
+///
+/// let checkboxes = CheckboxGroup::new()
+///     .set_action_id("group-0")
+///     .push_option(
+///         Opt::plain("option-0").set_value("value-0")
+///     )
+///     .push_option(
+///         Opt::plain("option-1").set_value("value-1")
+///     );
+///
+/// let expected = json!({
+///     "type": "checkboxes",
+///     "action_id": "group-0",
+///     "options": [
+///         {
+///             "value": "value-0",
+///             "text": {
+///                 "type": "plain_text",
+///                 "text": "option-0",
+///                 "emoji": true
+///             }
+///         },
+///         {
+///             "value": "value-1",
+///             "text": {
+///                 "type": "plain_text",
+///                 "text": "option-1",
+///                 "emoji": true
+///             }
+///         },
+///     ]
+/// });
+///
+/// let checkboxes_json = serde_json::to_value(checkboxes).unwrap();
+///
+/// assert_eq!(checkboxes_json, expected);
+/// ```
 #[derive(Debug, Serialize)]
 pub struct CheckboxGroup {
     #[serde(rename = "type")]
@@ -21,10 +66,28 @@ pub struct CheckboxGroup {
 }
 
 impl CheckboxGroup {
-    pub fn new<T: Into<String>>(action_id: T) -> Self {
+    /// Constructs a Checkbox group with empty values.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::CheckboxGroup;
+    /// use serde_json::json;
+    ///
+    /// let checkboxes = CheckboxGroup::new();
+    ///
+    /// let expected = json!({
+    ///     "type": "checkboxes",
+    ///     "action_id": "",
+    ///     "options": []
+    /// });
+    ///
+    /// let checkboxes_json = serde_json::to_value(checkboxes).unwrap();
+    ///
+    /// assert_eq!(checkboxes_json, expected);
+    /// ```
+    pub fn new() -> Self {
         Self {
             kind: "checkboxes",
-            action_id: action_id.into(),
+            action_id: "".into(),
             options: vec![],
             initial_options: vec![],
             confirm: None,
@@ -32,6 +95,24 @@ impl CheckboxGroup {
         }
     }
 
+    /// Sets action_id field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::CheckboxGroup;
+    /// use serde_json::json;
+    ///
+    /// let checkboxes = CheckboxGroup::new().set_action_id("group-0");
+    ///
+    /// let expected = json!({
+    ///     "type": "checkboxes",
+    ///     "action_id": "group-0",
+    ///     "options": []
+    /// });
+    ///
+    /// let checkboxes_json = serde_json::to_value(checkboxes).unwrap();
+    ///
+    /// assert_eq!(checkboxes_json, expected);
+    /// ```
     pub fn set_action_id<T: Into<String>>(self, action_id: T) -> Self {
         Self {
             action_id: action_id.into(),
@@ -39,16 +120,129 @@ impl CheckboxGroup {
         }
     }
 
+    /// Sets options field directly.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{CheckboxGroup, Opt};
+    /// use serde_json::json;
+    ///
+    /// let checkboxes = CheckboxGroup::new()
+    ///     .set_options(
+    ///         vec![
+    ///             Opt::plain("option-0").set_value("value-0"),
+    ///             Opt::plain("option-1").set_value("value-1"),
+    ///         ]
+    ///     );
+    ///
+    /// let expected = json!({
+    ///     "type": "checkboxes",
+    ///     "action_id": "",
+    ///     "options": [
+    ///         {
+    ///             "value": "value-0",
+    ///             "text": {
+    ///                 "type": "plain_text",
+    ///                 "text": "option-0",
+    ///                 "emoji": true
+    ///             }
+    ///         },
+    ///         {
+    ///             "value": "value-1",
+    ///             "text": {
+    ///                 "type": "plain_text",
+    ///                 "text": "option-1",
+    ///                 "emoji": true
+    ///             }
+    ///         }
+    ///     ]
+    /// });
+    ///
+    /// let checkboxes_json = serde_json::to_value(checkboxes).unwrap();
+    ///
+    /// assert_eq!(checkboxes_json, expected);
+    /// ```
     pub fn set_options(self, options: Vec<Opt>) -> Self {
         Self { options, ..self }
     }
 
+    /// Adds Opt object to options field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{CheckboxGroup, Opt};
+    /// use serde_json::json;
+    ///
+    /// let checkboxes = CheckboxGroup::new()
+    ///     .push_option(
+    ///         Opt::plain("option-0").set_value("value-0")
+    ///     );
+    ///
+    /// let expected = json!({
+    ///     "type": "checkboxes",
+    ///     "action_id": "",
+    ///     "options": [
+    ///         {
+    ///             "value": "value-0",
+    ///             "text": {
+    ///                 "type": "plain_text",
+    ///                 "text": "option-0",
+    ///                 "emoji": true
+    ///             }
+    ///         }
+    ///     ]
+    /// });
+    ///
+    /// let checkboxes_json = serde_json::to_value(checkboxes).unwrap();
+    ///
+    /// assert_eq!(checkboxes_json, expected);
+    /// ```
     pub fn push_option(self, option: Opt) -> Self {
         let Self { mut options, .. } = self;
         options.push(option);
         Self { options, ..self }
     }
 
+    /// Sets initial_options field directly.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{CheckboxGroup, Opt};
+    /// use serde_json::json;
+    ///
+    /// let checkboxes = CheckboxGroup::new()
+    ///     .set_initial_options(
+    ///         vec![
+    ///             Opt::plain("option-0").set_value("value-0"),
+    ///             Opt::plain("option-1").set_value("value-1"),
+    ///         ]
+    ///     );
+    ///
+    /// let expected = json!({
+    ///     "type": "checkboxes",
+    ///     "action_id": "",
+    ///     "options": [],
+    ///     "initial_options": [
+    ///         {
+    ///             "value": "value-0",
+    ///             "text": {
+    ///                 "type": "plain_text",
+    ///                 "text": "option-0",
+    ///                 "emoji": true
+    ///             }
+    ///         },
+    ///         {
+    ///             "value": "value-1",
+    ///             "text": {
+    ///                 "type": "plain_text",
+    ///                 "text": "option-1",
+    ///                 "emoji": true
+    ///             }
+    ///         }
+    ///     ]
+    /// });
+    ///
+    /// let checkboxes_json = serde_json::to_value(checkboxes).unwrap();
+    ///
+    /// assert_eq!(checkboxes_json, expected);
+    /// ```
     pub fn set_initial_options(self, initial_options: Vec<Opt>) -> Self {
         Self {
             initial_options,
@@ -56,6 +250,37 @@ impl CheckboxGroup {
         }
     }
 
+    /// Adds Opt object to initial_options field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{CheckboxGroup, Opt};
+    /// use serde_json::json;
+    ///
+    /// let checkboxes = CheckboxGroup::new()
+    ///     .push_initial_option(
+    ///         Opt::plain("option-0").set_value("value-0"),
+    ///     );
+    ///
+    /// let expected = json!({
+    ///     "type": "checkboxes",
+    ///     "action_id": "",
+    ///     "options": [],
+    ///     "initial_options": [
+    ///         {
+    ///             "value": "value-0",
+    ///             "text": {
+    ///                 "type": "plain_text",
+    ///                 "text": "option-0",
+    ///                 "emoji": true
+    ///             }
+    ///         }
+    ///     ]
+    /// });
+    ///
+    /// let checkboxes_json = serde_json::to_value(checkboxes).unwrap();
+    ///
+    /// assert_eq!(checkboxes_json, expected);
+    /// ```
     pub fn push_initial_option(self, initial_option: Opt) -> Self {
         let Self {
             mut initial_options,
@@ -68,6 +293,53 @@ impl CheckboxGroup {
         }
     }
 
+    /// Sets confirm field with ConfirmationDialog object.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{CheckboxGroup, ConfirmationDialog};
+    /// use serde_json::json;
+    ///
+    /// let checkboxes = CheckboxGroup::new()
+    ///     .set_confirm(
+    ///         ConfirmationDialog::new()
+    ///             .set_title("Are you sure?")
+    ///             .set_text("Wouldn't you prefer a good game of _chess_?")
+    ///             .set_confirm("Do it")
+    ///             .set_deny("Stop, I've changed my mind!")
+    ///     );
+    ///
+    /// let expected = json!({
+    ///     "type": "checkboxes",
+    ///     "action_id": "",
+    ///     "options": [],
+    ///     "confirm": {
+    ///         "title": {
+    ///             "type": "plain_text",
+    ///             "text": "Are you sure?",
+    ///             "emoji": true
+    ///         },
+    ///         "text": {
+    ///             "type": "plain_text",
+    ///             "text": "Wouldn't you prefer a good game of _chess_?",
+    ///             "emoji": true
+    ///         },
+    ///         "confirm": {
+    ///             "type": "plain_text",
+    ///             "text": "Do it",
+    ///             "emoji": true
+    ///         },
+    ///         "deny": {
+    ///             "type": "plain_text",
+    ///             "text": "Stop, I've changed my mind!",
+    ///             "emoji": true
+    ///         }
+    ///     }
+    /// });
+    ///
+    /// let checkboxes_json = serde_json::to_value(checkboxes).unwrap();
+    ///
+    /// assert_eq!(checkboxes_json, expected);
+    /// ```
     pub fn set_confirm(self, confirm: ConfirmationDialog) -> Self {
         Self {
             confirm: Some(confirm),
@@ -75,6 +347,25 @@ impl CheckboxGroup {
         }
     }
 
+    /// Sets focus_on_load field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{CheckboxGroup, Opt};
+    /// use serde_json::json;
+    ///
+    /// let checkboxes = CheckboxGroup::new().set_focus_on_load(true);
+    ///
+    /// let expected = json!({
+    ///     "type": "checkboxes",
+    ///     "action_id": "",
+    ///     "options": [],
+    ///     "focus_on_load": true
+    /// });
+    ///
+    /// let checkboxes_json = serde_json::to_value(checkboxes).unwrap();
+    ///
+    /// assert_eq!(checkboxes_json, expected);
+    /// ```
     pub fn set_focus_on_load(self, focus_on_load: bool) -> Self {
         Self {
             focus_on_load: Some(focus_on_load),
