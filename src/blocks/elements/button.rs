3a21fb59119174ae1113ac002d410ef9e34390fe
Kanji Tanaka <ktanaka@ichain.co.jp>
2023-02-25 09:28:26 +0900

    Fix Button

--- src/blocks/elements/button.rs
+++ src/blocks/elements/button.rs
@@ -1,6 +1,35 @@
 use super::{ConfirmationDialog, Text};
 use serde::Serialize;
 
+/// [Button element](https://api.slack.com/reference/block-kit/block-elements#button)
+/// representation.
+///
+/// # Example
+///
+/// ```
+/// use slack_messaging::blocks::elements::Button;
+/// use serde_json::json;
+///
+/// let button = Button::new()
+///     .text("Click Me")
+///     .set_value("click_me_123")
+///     .set_action_id("button-0");
+///
+/// let expected = json!({
+///     "type": "button",
+///     "text": {
+///         "type": "plain_text",
+///         "text": "Click Me",
+///         "emoji": true
+///     },
+///     "value": "click_me_123",
+///     "action_id": "button-0"
+/// });
+///
+/// let button_json = serde_json::to_value(button).unwrap();
+///
+/// assert_eq!(button_json, expected);
+/// ```
 #[derive(Debug, Serialize)]
 pub struct Button {
     #[serde(rename = "type")]
@@ -27,15 +56,33 @@ pub struct Button {
 }
 
 impl Button {
-    pub fn new<S, T>(text: S, action_id: T) -> Self
-    where
-        S: Into<String>,
-        T: Into<String>,
-    {
+    /// Constructs a Button element.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Button;
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new();
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": ""
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
+    pub fn new() -> Self {
         Self {
             kind: "button",
-            text: Text::plain(text),
-            action_id: action_id.into(),
+            text: Text::plain(""),
+            action_id: "".into(),
             url: None,
             value: None,
             style: None,
@@ -44,10 +91,80 @@ impl Button {
         }
     }
 
+    /// Sets text field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{Button, Text};
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new().set_text(Text::plain("Click Me"));
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "Click Me",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": ""
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
     pub fn set_text(self, text: Text) -> Self {
         Self { text, ..self }
     }
 
+    /// Sets text field from string. This is a shorthand for `set_text` method.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Button;
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new().text("Click Me");
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "Click Me",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": ""
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
+    pub fn text<T: Into<String>>(self, text: T) -> Self {
+        self.set_text(Text::plain(text))
+    }
+
+    /// Sets action_id field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Button;
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new().set_action_id("click_me_123");
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": "click_me_123"
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
     pub fn set_action_id<T: Into<String>>(self, action_id: T) -> Self {
         Self {
             action_id: action_id.into(),
@@ -55,6 +172,29 @@ impl Button {
         }
     }
 
+    /// Sets url field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Button;
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new().set_url("https://google.com");
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": "",
+    ///     "url": "https://google.com"
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
     pub fn set_url<T: Into<String>>(self, url: T) -> Self {
         Self {
             url: Some(url.into()),
@@ -62,6 +202,29 @@ impl Button {
         }
     }
 
+    /// Sets value field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Button;
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new().set_value("value-0");
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": "",
+    ///     "value": "value-0"
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
     pub fn set_value<T: Into<String>>(self, value: T) -> Self {
         Self {
             value: Some(value.into()),
@@ -69,6 +232,29 @@ impl Button {
         }
     }
 
+    /// Sets primary to style field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Button;
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new().set_primary();
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": "",
+    ///     "style": "primary"
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
     pub fn set_primary(self) -> Self {
         Self {
             style: Some("primary"),
@@ -76,6 +262,29 @@ impl Button {
         }
     }
 
+    /// Sets danger to style field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Button;
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new().set_danger();
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": "",
+    ///     "style": "danger"
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
     pub fn set_danger(self) -> Self {
         Self {
             style: Some("danger"),
@@ -83,6 +292,57 @@ impl Button {
         }
     }
 
+    /// Sets confirm field with ConfirmationDialog object.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{Button, ConfirmationDialog};
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new()
+    ///     .set_confirm(
+    ///         ConfirmationDialog::new()
+    ///             .set_title("Are you sure?")
+    ///             .set_text("Wouldn't you prefer a good game of _chess_?")
+    ///             .set_confirm("Do it")
+    ///             .set_deny("Stop, I've changed my mind!")
+    ///     );
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": "",
+    ///     "confirm": {
+    ///         "title": {
+    ///             "type": "plain_text",
+    ///             "text": "Are you sure?",
+    ///             "emoji": true
+    ///         },
+    ///         "text": {
+    ///             "type": "plain_text",
+    ///             "text": "Wouldn't you prefer a good game of _chess_?",
+    ///             "emoji": true
+    ///         },
+    ///         "confirm": {
+    ///             "type": "plain_text",
+    ///             "text": "Do it",
+    ///             "emoji": true
+    ///         },
+    ///         "deny": {
+    ///             "type": "plain_text",
+    ///             "text": "Stop, I've changed my mind!",
+    ///             "emoji": true
+    ///         }
+    ///     }
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
     pub fn set_confirm(self, confirm: ConfirmationDialog) -> Self {
         Self {
             confirm: Some(confirm),
@@ -90,6 +350,29 @@ impl Button {
         }
     }
 
+    /// Sets accessibility_label field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::Button;
+    /// use serde_json::json;
+    ///
+    /// let button = Button::new().set_accessibility_label("Click Me");
+    ///
+    /// let expected = json!({
+    ///     "type": "button",
+    ///     "text": {
+    ///         "type": "plain_text",
+    ///         "text": "",
+    ///         "emoji": true
+    ///     },
+    ///     "action_id": "",
+    ///     "accessibility_label": "Click Me"
+    /// });
+    ///
+    /// let button_json = serde_json::to_value(button).unwrap();
+    ///
+    /// assert_eq!(button_json, expected);
+    /// ```
     pub fn set_accessibility_label<T: Into<String>>(self, label: T) -> Self {
         Self {
             accessibility_label: Some(label.into()),
