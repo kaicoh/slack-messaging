85d1204a0ae58a9e7c2451800ba6502656baf6ec
Kanji Tanaka <ktanaka@ichain.co.jp>
2023-02-25 19:38:49 +0900

    Fix TimePicker

--- src/blocks/elements/timepicker.rs
+++ src/blocks/elements/timepicker.rs
@@ -1,6 +1,37 @@
 use super::{ConfirmationDialog, Text};
 use serde::Serialize;
 
+/// [Time picker element](https://api.slack.com/reference/block-kit/block-elements#timepicker)
+/// representation.
+///
+/// # Example
+///
+/// ```
+/// use slack_messaging::blocks::elements::TimePicker;
+/// use serde_json::json;
+///
+/// let timepicker = TimePicker::new()
+///     .set_action_id("timepicker123")
+///     .set_initial_time("11:30")
+///     .set_timezone("Asia/Tokyo")
+///     .placeholder("Select a time");
+///
+/// let expected = json!({
+///     "type": "timepicker",
+///     "action_id": "timepicker123",
+///     "initial_time": "11:30",
+///     "timezone": "Asia/Tokyo",
+///     "placeholder": {
+///         "type": "plain_text",
+///         "text": "Select a time",
+///         "emoji": true
+///     }
+/// });
+///
+/// let timepicker_json = serde_json::to_value(timepicker).unwrap();
+///
+/// assert_eq!(timepicker_json, expected);
+/// ```
 #[derive(Debug, Serialize)]
 pub struct TimePicker {
     #[serde(rename = "type")]
@@ -24,11 +55,11 @@ pub struct TimePicker {
     timezone: Option<String>,
 }
 
-impl TimePicker {
-    pub fn new<T: Into<String>>(action_id: T) -> Self {
+impl Default for TimePicker {
+    fn default() -> Self {
         Self {
             kind: "timepicker",
-            action_id: action_id.into(),
+            action_id: "".into(),
             initial_time: None,
             confirm: None,
             focus_on_load: None,
@@ -36,7 +67,47 @@ impl TimePicker {
             timezone: None,
         }
     }
+}
 
+impl TimePicker {
+    /// Constructs a Time picker element with empty values.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::TimePicker;
+    /// use serde_json::json;
+    ///
+    /// let timepicker = TimePicker::new();
+    ///
+    /// let expected = json!({
+    ///     "type": "timepicker",
+    ///     "action_id": ""
+    /// });
+    ///
+    /// let timepicker_json = serde_json::to_value(timepicker).unwrap();
+    ///
+    /// assert_eq!(timepicker_json, expected);
+    /// ```
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Sets action_id field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::TimePicker;
+    /// use serde_json::json;
+    ///
+    /// let timepicker = TimePicker::new().set_action_id("timepicker123");
+    ///
+    /// let expected = json!({
+    ///     "type": "timepicker",
+    ///     "action_id": "timepicker123"
+    /// });
+    ///
+    /// let timepicker_json = serde_json::to_value(timepicker).unwrap();
+    ///
+    /// assert_eq!(timepicker_json, expected);
+    /// ```
     pub fn set_action_id<T: Into<String>>(self, action_id: T) -> Self {
         Self {
             action_id: action_id.into(),
@@ -44,6 +115,24 @@ impl TimePicker {
         }
     }
 
+    /// Sets initial_time field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::TimePicker;
+    /// use serde_json::json;
+    ///
+    /// let timepicker = TimePicker::new().set_initial_time("11:00");
+    ///
+    /// let expected = json!({
+    ///     "type": "timepicker",
+    ///     "action_id": "",
+    ///     "initial_time": "11:00"
+    /// });
+    ///
+    /// let timepicker_json = serde_json::to_value(timepicker).unwrap();
+    ///
+    /// assert_eq!(timepicker_json, expected);
+    /// ```
     pub fn set_initial_time<T: Into<String>>(self, value: T) -> Self {
         Self {
             initial_time: Some(value.into()),
@@ -51,6 +140,52 @@ impl TimePicker {
         }
     }
 
+    /// Sets confirm field with ConfirmationDialog.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{TimePicker, ConfirmationDialog};
+    /// use serde_json::json;
+    ///
+    /// let timepicker = TimePicker::new()
+    ///     .set_confirm(
+    ///         ConfirmationDialog::new()
+    ///             .set_title("Are you sure?")
+    ///             .set_text("Wouldn't you prefer a good game of _chess_?")
+    ///             .set_confirm("Do it")
+    ///             .set_deny("Stop, I've changed my mind!")
+    ///     );
+    ///
+    /// let expected = json!({
+    ///     "type": "timepicker",
+    ///     "action_id": "",
+    ///     "confirm": {
+    ///         "title": {
+    ///             "type": "plain_text",
+    ///             "text": "Are you sure?",
+    ///             "emoji": true
+    ///         },
+    ///         "text": {
+    ///             "type": "plain_text",
+    ///             "text": "Wouldn't you prefer a good game of _chess_?",
+    ///             "emoji": true
+    ///         },
+    ///         "confirm": {
+    ///             "type": "plain_text",
+    ///             "text": "Do it",
+    ///             "emoji": true
+    ///         },
+    ///         "deny": {
+    ///             "type": "plain_text",
+    ///             "text": "Stop, I've changed my mind!",
+    ///             "emoji": true
+    ///         }
+    ///     }
+    /// });
+    ///
+    /// let timepicker_json = serde_json::to_value(timepicker).unwrap();
+    ///
+    /// assert_eq!(timepicker_json, expected);
+    /// ```
     pub fn set_confirm(self, confirm: ConfirmationDialog) -> Self {
         Self {
             confirm: Some(confirm),
@@ -58,6 +193,24 @@ impl TimePicker {
         }
     }
 
+    /// Sets focus_on_load field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::TimePicker;
+    /// use serde_json::json;
+    ///
+    /// let timepicker = TimePicker::new().set_focus_on_load(true);
+    ///
+    /// let expected = json!({
+    ///     "type": "timepicker",
+    ///     "action_id": "",
+    ///     "focus_on_load": true
+    /// });
+    ///
+    /// let timepicker_json = serde_json::to_value(timepicker).unwrap();
+    ///
+    /// assert_eq!(timepicker_json, expected);
+    /// ```
     pub fn set_focus_on_load(self, focus_on_load: bool) -> Self {
         Self {
             focus_on_load: Some(focus_on_load),
@@ -65,6 +218,29 @@ impl TimePicker {
         }
     }
 
+    /// Sets placeholder field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::{TimePicker, Text};
+    /// use serde_json::json;
+    ///
+    /// let timepicker = TimePicker::new()
+    ///     .set_placeholder(Text::plain("Select a time"));
+    ///
+    /// let expected = json!({
+    ///     "type": "timepicker",
+    ///     "action_id": "",
+    ///     "placeholder": {
+    ///         "type": "plain_text",
+    ///         "text": "Select a time",
+    ///         "emoji": true
+    ///     }
+    /// });
+    ///
+    /// let timepicker_json = serde_json::to_value(timepicker).unwrap();
+    ///
+    /// assert_eq!(timepicker_json, expected);
+    /// ```
     pub fn set_placeholder(self, placeholder: Text) -> Self {
         Self {
             placeholder: Some(placeholder),
@@ -72,10 +248,50 @@ impl TimePicker {
         }
     }
 
+    /// Sets placeholder field from string. This is a shorthand for `set_placeholder` method.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::TimePicker;
+    /// use serde_json::json;
+    ///
+    /// let timepicker = TimePicker::new().placeholder("Select a time");
+    ///
+    /// let expected = json!({
+    ///     "type": "timepicker",
+    ///     "action_id": "",
+    ///     "placeholder": {
+    ///         "type": "plain_text",
+    ///         "text": "Select a time",
+    ///         "emoji": true
+    ///     }
+    /// });
+    ///
+    /// let timepicker_json = serde_json::to_value(timepicker).unwrap();
+    ///
+    /// assert_eq!(timepicker_json, expected);
+    /// ```
     pub fn placeholder<T: Into<String>>(self, placeholder: T) -> Self {
         self.set_placeholder(Text::plain(placeholder))
     }
 
+    /// Sets timezone field.
+    ///
+    /// ```
+    /// use slack_messaging::blocks::elements::TimePicker;
+    /// use serde_json::json;
+    ///
+    /// let timepicker = TimePicker::new().set_timezone("Asia/Tokyo");
+    ///
+    /// let expected = json!({
+    ///     "type": "timepicker",
+    ///     "action_id": "",
+    ///     "timezone": "Asia/Tokyo"
+    /// });
+    ///
+    /// let timepicker_json = serde_json::to_value(timepicker).unwrap();
+    ///
+    /// assert_eq!(timepicker_json, expected);
+    /// ```
     pub fn set_timezone<T: Into<String>>(self, value: T) -> Self {
         Self {
             timezone: Some(value.into()),
